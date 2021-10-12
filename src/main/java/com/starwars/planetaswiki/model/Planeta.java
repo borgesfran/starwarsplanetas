f5e2c6c11b2473939a4e9edbc36c2e121c90aeef
francinette <francinette.borges@gmail.com>
2021-10-11 21:14:45 -0300

    D - Adicionada descrições para o schema Planeta

--- src/main/java/com/starwars/planetaswiki/model/Planeta.java
+++ src/main/java/com/starwars/planetaswiki/model/Planeta.java
@@ -2,6 +2,7 @@ package com.starwars.planetaswiki.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,19 +16,26 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Representação abstrata de um planeta do Universo Star Wars")
 @Document
 public class Planeta {
 
     @Id
     private String id;
 
+    @Schema(description = "Nome do planeta")
     @Indexed(unique = true)
     private String nome;
 
+    @Schema(description = "Clima do planeta")
     private String clima;
 
+    @Schema(description = "Terreno do planeta")
     private String terreno;
 
+    @Schema(description = "Quantidade de aparições do planeta nos filmes da saga. " +
+            "Esta informação é preenchida sempre ao se cadastrar um novo planeta ou ao atualizar um já existente, " +
+            "com base nas informações disponíveis em https://swapi.dev/about.")
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @Transient
     private int quantidadeAparicoes;
---
+++ src/test/java/com/starwars/planetaswiki/PlanetaswikiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.starwars.planetaswiki;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class PlanetaswikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
